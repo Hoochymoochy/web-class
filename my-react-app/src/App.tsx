849b645af08ffeb55aa16205d7352f63977094c2
Hoochymoochy <Khaydenroberts22@gmail.com>
2026-01-29 17:29:13 -0700

    added routes

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,92 +1,18 @@
-import { useEffect, useState } from 'react'
-import Header from './Components/Header'
-import TaskList from './Components/TaskList'
-
-export type Task = {
-  id: number
-  title: string
-  description: string
-  priority: number
-  dueDate: string
-  completed: boolean
-}
-
+// App.js
+import { Routes, Route } from 'react-router-dom';
+import Home from './page/home';
+import About from './page/about';
+import Task from './page/task';
 function App() {
-  const [tasks, setTasks] = useState<Task[]>(() => {
-    const savedTasks = localStorage.getItem('tasks')
-    return savedTasks ? JSON.parse(savedTasks) : []
-  })
-
-  useEffect(() => {
-    setLoading(false)
-  }, [])
-  
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [priority, setPriority] = useState<number | ''>('')
-  const [dueDate, setDueDate] = useState('')
-  const [loading, setLoading] = useState(true)
-
-  const handleAddTask = () => {
-    if (!title || !description || priority === '' || !dueDate) return
-
-    const newTask: Task = {
-      id: tasks.length + 1,
-      title,
-      description,
-      priority,
-      dueDate,
-      completed: false,
-    }
-
-    setTasks(prev => [...prev, newTask])
-
-    setTitle('')
-    setDescription('')
-    setPriority('')
-    setDueDate('')
-  }
-
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-  }, [tasks])
-
-  const handleCompleteTask = (id: number) => {
-    setTasks(prev =>
-      prev.map(task =>
-        task.id === id
-          ? { ...task, completed: !task.completed }
-          : task
-      )
-    )
-  }
-
-  const handleDeleteTask = (id: number) => {
-    setTasks(prev => prev.filter(task => task.id !== id))
-  }
-
   return (
-    <div className='flex justify-center flex-col items-center p-4'>
-      <Header />
-
-
-      <form onSubmit={e => e.preventDefault()}>
-        <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)}/>
-        <input type="text" placeholder="Description" value={description} onChange={e => setDescription(e.target.value)}/>
-        <input type="number" placeholder="Priority" value={priority} onChange={e => setPriority(Number(e.target.value))} />
-        <input type="date" value={dueDate} onChange={e => setDueDate(e.target.value)}/>
-        <button type="button" onClick={handleAddTask}>Add Task</button>
-      </form>
-
-      <TaskList
-        tasks={tasks}
-        onDelete={handleDeleteTask}
-        handleCompleteTask={handleCompleteTask}
-      />
-            {loading && <p>Loading...</p>}
-
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/task" element={<Task />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/page/Home.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react'
+import Header from '../Components/Header'
+import TaskList from '../Components/TaskList'
+
+export type Task = {
+  id: number
+  title: string
+  description: string
+  priority: number
+  dueDate: string
+  completed: boolean
+}
+
+function Home() {
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const savedTasks = localStorage.getItem('tasks')
+    return savedTasks ? JSON.parse(savedTasks) : []
+  })
+
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+  
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [priority, setPriority] = useState<number | ''>('')
+  const [dueDate, setDueDate] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  const handleAddTask = () => {
+    if (!title || !description || priority === '' || !dueDate) return
+
+    const newTask: Task = {
+      id: tasks.length + 1,
+      title,
+      description,
+      priority,
+      dueDate,
+      completed: false,
+    }
+
+    setTasks(prev => [...prev, newTask])
+
+    setTitle('')
+    setDescription('')
+    setPriority('')
+    setDueDate('')
+  }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
+  const handleCompleteTask = (id: number) => {
+    setTasks(prev =>
+      prev.map(task =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    )
+  }
+
+  const handleDeleteTask = (id: number) => {
+    setTasks(prev => prev.filter(task => task.id !== id))
+  }
+
+  return (
+    <div className='flex justify-center flex-col items-center p-4'>
+      <Header />
+
+
+      <form onSubmit={e => e.preventDefault()}>
+        <input type="text" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)}/>
+        <input type="text" placeholder="Description" value={description} onChange={e => setDescription(e.target.value)}/>
+        <input type="number" placeholder="Priority" value={priority} onChange={e => setPriority(Number(e.target.value))} />
+        <input type="date" value={dueDate} onChange={e => setDueDate(e.target.value)}/>
+        <button type="button" onClick={handleAddTask}>Add Task</button>
+      </form>
+
+      <TaskList
+        tasks={tasks}
+        onDelete={handleDeleteTask}
+        handleCompleteTask={handleCompleteTask}
+      />
+            {loading && <p>Loading...</p>}
+
+    </div>
+  )
+}
+
+export default Home
